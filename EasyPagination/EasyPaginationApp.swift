ca0b2e4fd8da13c0be813c72cab2eb00c8c6d266
Сергей Климович <sergei.klimovich@gmail.com>
2021-02-23 10:38:00 +0300

    Initial Commit

---
+++ EasyPagination/EasyPaginationApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyPaginationApp.swift
+//  EasyPagination
+//
+//  Created by Администратор on 23.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct EasyPaginationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
